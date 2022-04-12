59f4a7b53eeea57ef3a3af3c814c2cfd943e16b1
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-04-12 17:00:23 +0100

    Added Recycler View with Custom Layout

--- app/src/main/java/com/example/kotlinvideoapp/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinvideoapp/MainAdapter.kt
@@ -1,25 +1,32 @@
 package com.example.kotlinvideoapp
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
 
+    val videoTitles = listOf<String>("First Video", "Second Video", "Third Video", "Fourth Video")
+
 //    number of items
     override fun getItemCount(): Int {
-        return 3
+        return videoTitles.size
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ??? {
-
+    override fun onCreateViewHolder(view: ViewGroup, index: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(view?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.videorow, view, false)
+        return CustomViewHolder(cellForRow)
     }
 
-    override fun onBindViewHolder(p0: ???, p1: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: CustomViewHolder, index: Int) {
+        val videoTitle = videoTitles.get(index)
+        holder.view.findViewById<TextView>(R.id.textView_videoTitle).text = videoTitle
     }
 }
 
-class CustomViewHolder(v: View): RecyclerView.ViewHolder(v) {
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
 }
